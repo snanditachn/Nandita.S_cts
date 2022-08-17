c21b9fef1204fe504117f197def713494fea48f2
Nandita <61895105+snanditachn@users.noreply.github.com>
2022-08-17 12:28:57 +0530

    Add files via upload

---
+++ greaterscript.py
@@ -0,0 +1,9 @@
+def find_greater():
+  x1 = input('Enter first number: ')
+  x2 = input('Enter second number: ')
+  if x1 > x2:
+    print('The larger number is '+x1)
+  else:
+    print('The larger number is '+x2)
+
+find_greater()
---
+++ smallerscript.py
@@ -0,0 +1,10 @@
+
+def find_smaller():
+  x1 = input('Enter first number: ')
+  x2 = input('Enter second number: ')
+  if x1 < x2:
+    print('The smaller number is '+x1)
+  else:
+    print('The smaller number is '+x2)
+
+find_smaller()
